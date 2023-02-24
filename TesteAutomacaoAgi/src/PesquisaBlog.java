f44d6ab14a69c005e99ab310900e46d706af6c1e
Bruno Lucas <brunolucas013@hotmail.com>
2023-02-24 09:38:36 -0300

    Alteração no metodo de busca xpath para cssSelector em dois testes - testes

--- TesteAutomacaoAgi/src/PesquisaBlog.java
+++ TesteAutomacaoAgi/src/PesquisaBlog.java
@@ -1,4 +1,16 @@
 package TesteAutomacaoAgi.src;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.openqa.selenium.By.className;
+import static org.openqa.selenium.By.id;
+
 public class PesquisaBlog {
+
+
 }
--- TesteAutomacaoAgi/src/PesquisaBlogTest.java
+++ TesteAutomacaoAgi/src/PesquisaBlogTest.java
@@ -26,36 +26,35 @@ class PesquisaBlogTest {
         navegador.navigate().to("https://blogdoagi.com.br/");
         navegador.findElement(id("overlay-open")).click();
         navegador.findElement(className("mobile-search")).click();
-        navegador.findElement(By.xpath("//*[@id=\"masthead\"]/div[3]/div[2]/form/label/input")).sendKeys(palavraChaveSucesso);
-        navegador.findElement(By.xpath("//*[@id=\"masthead\"]/div[3]/div[2]/form/label/input")).submit();
+        navegador.findElement(By.cssSelector(".mobile-search .search-field")).sendKeys(palavraChaveSucesso);
+        navegador.findElement(By.cssSelector(".mobile-search .search-field")).submit();
 
         WebElement resultadoDaBusca = navegador.findElement(By.className("archive-title"));
         String expectedText = String.format("Resultados da busca por: %s", palavraChaveSucesso);
         Assert.assertEquals(expectedText, resultadoDaBusca.getText());
 
         System.out.println("Teste com sucesso");
-        navegador.quit();
+
     }
 
 
     @Test
-    public void clicarNoMenuMobileClicarNaLupaPesquisarPorDataDePublicacaoTesteMobile() {
+    public void clicarNoMenuMobileClicarNaLupaPesquisarPorDataDePublicacaoNaoEncontraTesteMobile() {
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         WebDriver navegador = new ChromeDriver();
-
 
         navegador.navigate().to("https://blogdoagi.com.br/");
         navegador.findElement(id("overlay-open")).click();
         navegador.findElement(className("mobile-search")).click();
-        navegador.findElement(By.xpath("//*[@id=\"masthead\"]/div[3]/div[2]/form/label/input")).sendKeys(PalavraChaveErro);
-        navegador.findElement(By.xpath("//*[@id=\"masthead\"]/div[3]/div[2]/form/label/input")).submit();
+        navegador.findElement(By.cssSelector(".mobile-search .search-field")).sendKeys(PalavraChaveErro);
+        navegador.findElement(By.cssSelector(".mobile-search .search-field")).submit();
 
         WebElement resultadoDaBusca = navegador.findElement(By.className("entry-title"));
         String expectedText = String.format("Nenhum resultado");
         Assert.assertEquals(expectedText, resultadoDaBusca.getText());
 
         System.out.println("Teste com sucesso");
-        navegador.quit();
+
     }
 
 
@@ -74,11 +73,10 @@ class PesquisaBlogTest {
         Assert.assertEquals(expectedText, resultadoDaBusca.getText());
 
         System.out.println("Teste com sucesso");
-        navegador.quit();
     }
 
     @Test
-    public void clicarNaLupaPesquisarPorDataDePublicacao() {
+    public void clicarNaLupaPesquisarPorDataDePublicacaoNaoEncontra() {
         // Localiza a caixa de pesquisa e insere um termo inexistente
         System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
         WebDriver navegador = new ChromeDriver();
@@ -93,7 +91,7 @@ class PesquisaBlogTest {
         String expectedText = String.format("Nenhum resultado");
         Assert.assertEquals(expectedText, resultadoDaBusca.getText());
         System.out.println("Teste com sucesso quando clicarNaLupaPesquisarPorDataDePublicacao");
-        navegador.quit();
+
     }
 
 }
